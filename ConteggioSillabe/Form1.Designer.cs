6eadd854b3d7c477c6df431cc8605852f0265156
unknown <info@archistico.com>
2019-06-06 17:59:01 +0200

    iniziale

---
+++ ConteggioSillabe/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace ConteggioSillabe
+{
+    partial class frmPrincipale
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmPrincipale));
+            this.tbInput = new System.Windows.Forms.TextBox();
+            this.btCalcola = new System.Windows.Forms.Button();
+            this.tbOutput = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // tbInput
+            // 
+            this.tbInput.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbInput.Location = new System.Drawing.Point(12, 12);
+            this.tbInput.Multiline = true;
+            this.tbInput.Name = "tbInput";
+            this.tbInput.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.tbInput.Size = new System.Drawing.Size(653, 228);
+            this.tbInput.TabIndex = 0;
+            this.tbInput.Text = resources.GetString("tbInput.Text");
+            // 
+            // btCalcola
+            // 
+            this.btCalcola.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btCalcola.Location = new System.Drawing.Point(12, 246);
+            this.btCalcola.Name = "btCalcola";
+            this.btCalcola.Size = new System.Drawing.Size(653, 48);
+            this.btCalcola.TabIndex = 1;
+            this.btCalcola.Text = "CALCOLA NUMERO SILLABE";
+            this.btCalcola.UseVisualStyleBackColor = true;
+            this.btCalcola.Click += new System.EventHandler(this.btCalcola_Click);
+            // 
+            // tbOutput
+            // 
+            this.tbOutput.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbOutput.Location = new System.Drawing.Point(12, 300);
+            this.tbOutput.Multiline = true;
+            this.tbOutput.Name = "tbOutput";
+            this.tbOutput.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.tbOutput.Size = new System.Drawing.Size(653, 259);
+            this.tbOutput.TabIndex = 2;
+            // 
+            // frmPrincipale
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(678, 571);
+            this.Controls.Add(this.tbOutput);
+            this.Controls.Add(this.btCalcola);
+            this.Controls.Add(this.tbInput);
+            this.Name = "frmPrincipale";
+            this.Text = "Calcola sillabe - Studio Archistico";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbInput;
+        private System.Windows.Forms.Button btCalcola;
+        private System.Windows.Forms.TextBox tbOutput;
+    }
+}
+
---
+++ ConteggioSillabe/Form1.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ConteggioSillabe
+{
+    public partial class frmPrincipale : Form
+    {
+        public frmPrincipale()
+        {
+            InitializeComponent();
+        }
+
+        private List<char> Vocali = new List<char>();
+        private List<char> Consonanti = new List<char>();
+        private List<char> LetterePermesse = new List<char>();
+
+        private string Calcola(string input)
+        {
+            // caratteri di punteggiatura
+            char[] interruzioni = { ' ', ',', '.', '\'', '\"', ';', ':', '-', '+', '/', '*', '!', '(', ')', '=', '?', '[', ']', '{', '}', '“', '”', '’', '»', '«', '%', '€' };
+
+            // Dividi il testo in parole
+            List<string> parole = input.ToLower().Split(interruzioni).ToList<string>();
+
+            // Elimina eventuali parole vuote
+            parole = parole.Where(s => !string.IsNullOrWhiteSpace(s)).ToList();
+
+            // Elimina parole con numeri o simboli
+            parole = parole.Where(s => s.Any(char.IsLetter)).ToList();
+
+            for (int i = 0; i < parole.Count; i++)
+            {
+                parole[i] = parole[i].PadRight(30) + Struttura(parole[i]);
+            }
+
+            // Unisci il tutto in una stringa e restituisci il risultato
+            string output = string.Join(Environment.NewLine, parole);
+            return output;
+        }
+
+        private string Struttura(string input)
+        {
+            StringBuilder parola = new StringBuilder(input);
+
+            for (int c = 0; c < parola.Length; c++)
+            {
+                if (parola[c].ToString().Any(x => Vocali.Contains(x)))
+                {
+                    parola[c] = 'V';
+                }
+                else
+                {
+                    parola[c] = 'C';
+                }
+            }
+
+            return parola.ToString();
+        }
+
+        private void btCalcola_Click(object sender, EventArgs e)
+        {
+            string input = "";
+            string output = "";
+
+            GeneraleCaricaLettere();
+
+            input = tbInput.Text;
+            output = Calcola(input);
+            tbOutput.Text = output;
+        }
+
+        private void GeneraleCaricaLettere()
+        {
+            Vocali.Add('a');
+            Vocali.Add('e');
+            Vocali.Add('i');
+            Vocali.Add('o');
+            Vocali.Add('u');
+
+            Vocali.Add('à');
+            Vocali.Add('é');
+            Vocali.Add('è');
+            Vocali.Add('ì');
+            Vocali.Add('ò');
+            Vocali.Add('ù');
+
+            Consonanti.Add('q');
+            Consonanti.Add('w');
+            Consonanti.Add('r');
+            Consonanti.Add('t');
+            Consonanti.Add('y');
+            Consonanti.Add('p');
+            Consonanti.Add('s');
+            Consonanti.Add('d');
+            Consonanti.Add('f');
+            Consonanti.Add('g');
+            Consonanti.Add('h');
+            Consonanti.Add('j');
+            Consonanti.Add('k');
+            Consonanti.Add('l');
+            Consonanti.Add('z');
+            Consonanti.Add('x');
+            Consonanti.Add('c');
+            Consonanti.Add('v');
+            Consonanti.Add('b');
+            Consonanti.Add('n');
+            Consonanti.Add('m');
+
+            LetterePermesse.Add('a');
+            LetterePermesse.Add('e');
+            LetterePermesse.Add('i');
+            LetterePermesse.Add('o');
+            LetterePermesse.Add('u');
+            LetterePermesse.Add('à');
+            LetterePermesse.Add('é');
+            LetterePermesse.Add('è');
+            LetterePermesse.Add('ì');
+            LetterePermesse.Add('ò');
+            LetterePermesse.Add('ù');
+            LetterePermesse.Add('q');
+            LetterePermesse.Add('w');
+            LetterePermesse.Add('r');
+            LetterePermesse.Add('t');
+            LetterePermesse.Add('y');
+            LetterePermesse.Add('p');
+            LetterePermesse.Add('s');
+            LetterePermesse.Add('d');
+            LetterePermesse.Add('f');
+            LetterePermesse.Add('g');
+            LetterePermesse.Add('h');
+            LetterePermesse.Add('j');
+            LetterePermesse.Add('k');
+            LetterePermesse.Add('l');
+            LetterePermesse.Add('z');
+            LetterePermesse.Add('x');
+            LetterePermesse.Add('c');
+            LetterePermesse.Add('v');
+            LetterePermesse.Add('b');
+            LetterePermesse.Add('n');
+            LetterePermesse.Add('m');
+        }
+
+
+
+    }
+}
+
+
